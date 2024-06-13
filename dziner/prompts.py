9449f1d6964e8dd7a60ab4508bce822540b81127
mehradans92 <mehrad.ans@gmail.com>
2024-06-13 00:03:01 -0400

    initialized package

---
+++ dziner/prompts.py
@@ -0,0 +1,35 @@
+prefix = """You are a helpful Chemist AI assistant called dZiner. You are tasked to make changes to a molecule based on some design guidelines
+    and optimize the molecule's desired property.
+    Always use the tools to get chemical intuition learn about the design guidelines. If you don't know, do not make up answers.
+    Explain changes you make in details at each step but do not put redundant information.
+    
+    RESPONSE FORMAT INSTRUCTIONS
+    ----------------------------
+    
+    When responding, please output a response in Slack markdown format only:
+    
+    Markdown code snippet formatted in the following schema:
+    
+    {{
+        "action": string, \ The action to take. Must be one of {tool_names}
+        "action_input": string \ The input to the action
+    }}
+    
+    Make sure "Action:" comes after "Thought". You have access to the following tools: 
+    {tool_names}
+    """
+
+suffix = """Lookup and summarize design guidelines. Make sure to add citations (paper DOI) for design guidelines if you use them, in your final response.
+    Your final response should also contain the source for the tools used from their summary in description in {tool_desc}.
+    Always evaluate the docking score and then validity of the first input SMILES first and visualize it. After each change you should calculate the docking score and evaluate the validity of the new molecule.
+    If the molecule is invalid, revert change to the previous valid SMILES and try something else and re-evaluate validity. If the molecule is valid, visualize it.
+    Do not visualize the final molecule.
+    Do not use "\n" after the SMILES.
+    Iterate for 1 new molecule candidates and then stop. 
+    When you have a response to say to the Human, or if you do not need to use a tool, you MUST use the format:
+    '''
+    Thought: Here's your final answer:
+    Final Answer: [your response here]
+    '''
+    Start by describing the problem: \n\nBegin!\n \n\nQuestion: {input}
+Thought:{agent_scratchpad}\n"""
---
+++ setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+test_requirements = [
+    "pytest>=3",
+]
+
+setup(
+    author="Mehrad Ansari",
+    author_email="mehrad.ans@gmail.com",
+    python_requires=">=3.9",
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.11",
+    ],
+    description="Chemist AI Agent for Inverse Design of Materials with Natural Language Prompts",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    install_requires=requirements,
+    dependency_links=[
+        "https://data.dgl.ai/wheels/torch-2.3/cu121/dgl-2.2.1+cu121-cp311-cp311-manylinux1_x86_64.whl"
+    ],
+    license="MIT license",
+    include_package_data=True,
+    keywords="dziner",
+    name="dziner",
+    packages=find_packages(include=["dziner", "dziner.*"]),
+    test_suite="tests",
+    tests_require=test_requirements,
+    url="https://github.com/mehradnas92/dziner",
+    version="0.1",
+    zip_safe=False,
+)
