ddf9668cdebf9a63ccb4440db76183fa60427f17
mehradans92 <mehrad.ans@gmail.com>
2024-09-19 16:29:34 -0400

    fixed except token limit except error in retrieval

--- dziner/__init__.py
+++ dziner/__init__.py
@@ -4,9 +4,5 @@ __author__ = """Mehrad Ansari"""
 __email__ = "mehrad.ans@gmail.com"
 __version__ = "0.1"
 
-# from .docs_utils.docs import *
 from .agents import *
 from .utils import *
-# from .general import *
-# from .parser import *
-# from .tools import *
--- dziner/utils.py
+++ dziner/utils.py
@@ -1,28 +1,31 @@
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import RetrievalQA
+from openai import OpenAIError
 
-def RetrievalQABypassTokenLimit(vector_store, RetrievalQA_prompt, llm, k=10, fetch_k=50, min_k=4, chain_type="stuff"):
+def RetrievalQABypassTokenLimit(vector_store, RetrievalQA_prompt, llm, k=10, fetch_k=50, min_k=2, chain_type="stuff"):
     while k >= min_k:
-            try:
-                retriever = vector_store.as_retriever(
-                    search_type="mmr",
-                    search_kwargs={"k": k, "fetch_k": fetch_k},
-                )
-                qa_chain = RetrievalQA.from_chain_type(
-                    llm=llm,
-                    chain_type=chain_type,
-                    retriever=retriever,
-                    memory=ConversationBufferMemory(
-                            ))
-                
-                # Check to see if we hit the token limit
-                result = qa_chain.run({"query": RetrievalQA_prompt})
-                return result  # If successful, return the result and exit the function
-    
-            except Exception as e:
-                # If an error is caught, reduce the value of k and retry
-                # print(e)
-                print(
-                    f"\nk={k} results hitting the token limit for the chosen LLM. Reducing k and retrying...\n"
-                )
+        try:
+            retriever = vector_store.as_retriever(
+                search_type="mmr",
+                search_kwargs={"k": k, "fetch_k": fetch_k},
+            )
+            qa_chain = RetrievalQA.from_chain_type( 
+                llm=llm,
+                chain_type=chain_type,
+                retriever=retriever,
+                memory=ConversationBufferMemory(
+                        ))
+            
+            # Check to see if we hit the token limit
+            result = qa_chain.run({"query": RetrievalQA_prompt})
+            return result  # If successful, return the result and exit the function
+
+        except OpenAIError as e:
+            # Check if it's a token limit error
+            print(e)
+            if 'maximum context length' in str(e):
+                print(f"\nk={k} results hitting the token limit. Reducing k and retrying...\n")
                 k -= 1
+            else:
+                # Re-raise other OpenAI errors
+                raise e
