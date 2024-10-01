0b69badc1347303622d892bbf119929eb9a00eb6
mehradans92 <mehrad.ans@gmail.com>
2024-09-30 22:24:24 -0400

    added few agent tools

--- dziner/tools.py
+++ dziner/tools.py
@@ -1,4 +1,8 @@
 from langchain.agents import tool
+import dziner.sascorer
+from rdkit import Chem
+from rdkit.Chem import QED
+from rdkit.Chem import Descriptors
 import os
 
 
@@ -33,3 +37,44 @@ def search(query):
     search = GoogleSerperAPIWrapper(engine='google', serpapi_api_key=os.getenv("SERPER_API_KEY"))
     results = search.run(query)
     return results
+
+@tool
+def mol_chemical_feasibility(smiles: str):
+    '''
+    This tool inputs a SMILES of a molecule and outputs chemical feasibility, Synthetic Accessibility (SA) scores,
+      molecular weight, and the SMILES. SA the ease of synthesis of compounds according to their synthetic complexity
+        which combines starting materials information and structural complexity. Lower SA, means better synthesiability.
+    '''
+    smiles = smiles.replace("\n", "")
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return "Invalid SMILES", 0, 0, smiles
+
+    # Calculate SA score
+    sa_score = dziner.sascorer.calculateScore(mol)
+
+    # Optionally calculate QED score
+    molecular_weight = Descriptors.MolWt(mol)
+    return "Valid SMILES", sa_score, molecular_weight, smiles
+
+@tool
+def drug_chemical_feasibility(smiles: str):
+    '''
+    This tool inputs a SMILES of a drug candidate and outputs chemical feasibility, Synthetic Accessibility (SA),
+      Quantentive drug-likeliness(QED) scores, molecular weight, and the SMILES. SA the ease of synthesis of compounds
+        according to their synthetic complexity which combines starting materials information and structural complexity. 
+        Lower SA, means better synthesiability. QED combines eight physicochemical properties(molecular weight, LogP, 
+        H-bond donors, H-bond acceptors, charge, aromaticity, stereochemistry and solubility), generating a score between 0 and 1.
+    '''
+    smiles = smiles.replace("\n", "")
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return "Invalid SMILES", 0, 0, smiles
+
+    # Calculate SA score
+    sa_score = dziner.sascorer.calculateScore(mol)
+
+    # Optionally calculate QED score
+    molecular_weight = Descriptors.MolWt(mol)
+    qed_score = QED.qed(mol)
+    return "Valid SMILES", sa_score, molecular_weight, qed_score, smiles
